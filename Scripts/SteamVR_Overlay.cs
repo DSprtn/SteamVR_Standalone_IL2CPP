c9f80a44362252c7bb327f7f471d50f8cf4d3169
Roughy <mdawn700@gmail.com>
2023-05-31 20:29:54 +0500

    hide crashing methods from il2cpp

--- Scripts/SteamVR_Overlay.cs
+++ Scripts/SteamVR_Overlay.cs
@@ -132,6 +132,7 @@ namespace Valve.VR
             }
         }
 
+        [HideFromIl2Cpp]
         public bool PollNextEvent(ref VREvent_t pEvent)
         {
             var overlay = OpenVR.Overlay;
@@ -150,6 +151,7 @@ namespace Valve.VR
             public float distance;
         }
 
+        [HideFromIl2Cpp]
         public bool ComputeIntersection(Vector3 source, Vector3 direction, ref IntersectionResults results)
         {
             var overlay = OpenVR.Overlay;
